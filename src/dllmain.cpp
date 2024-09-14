dedb8c1356db9be7da813fe025c188c084da6885
Lyall Stamper <lyall@stamper.net>
2024-09-14 01:40:39 +0100

    Version bump.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -25,7 +25,7 @@ HMODULE baseModule = GetModuleHandle(NULL);
 
 // Version
 std::string sFixName = "SMTVFix";
-std::string sFixVer = "0.9.8b";
+std::string sFixVer = "0.9.9";
 std::string sLogFile = sFixName + ".log";
 
 // Logger
